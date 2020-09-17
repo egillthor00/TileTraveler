ca275ccd6e7652ff5e9fcbee161a73965d8ebf23
Egill Þór Olgeirsson <inga@Egills-MacBook-Pro.local>
2020-09-17 11:07:58 +0000

    almost done

--- TileTraveler.py
+++ TileTraveler.py
@@ -1,49 +1,82 @@
 position = 11
-
+travel = ""
 while position != 31:
     if position == 11:
         print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
-    elif travel == "n":
+        travel = input("Direction ")
+        travel = travel.lower()
+        if travel == "n":
             position += 1
         else:
             print("Not a valid direction!")
     elif position == 12:
         print("You can travel: (N)orth or (E)ast or (S)outh.")
-        travel = input.lower("Direction")
-        elif travel == "n" or travel == "e" or travel == "s":
-            elif travel == "n":
+        travel = input("Direction ")
+        travel = travel.lower()
+        if travel == "n" or travel == "e" or travel == "s":
+            if travel == "n":
                 position +=1
-
+            elif travel == "e":
+                position += 10
+            elif travel == "s":
+                position -= 1
         else:
             print("not a valid direction!")
 
     elif position == 13:
         print("You can travel: (E)ast or (S)outh.")
-        travel = input.lower("Direction")
-        elif  travel == "e" or travel == "s":
-            elif travel == "n":
-                position +=1  
+        travel = input("Direction ")
+        travel = travel.lower()
+        if  travel == "e" or travel == "s":
+            if travel == "e":
+                position += 10
+            elif travel == "s":
+                position -= 1
+        else:
+            print("not a valid direction!")
 
     elif position == 21:
         print("You can travel: (E)ast or (W)est.")
-        travel = input.lower("Direction")
+        travel = input("Direction ")
+        travel = travel.lower()
+        if travel == "n":
+            position += 1
+        else:
+            print("not a valid direction!")
 
     elif position == 22:
         print("You can travel: (S)outh or (W)est.")
-        travel = input.lower("Direction")
+        travel = input("Direction ")
+        travel = travel.lower()
+        if travel == "s" or travel == "w":
+            if travel == "s":
+                position -=1
+            elif travel == "w":
+                position -= 10
+        else:
+            print("not a valid direction!")
 
     elif position == 23:
         print("You can travel: (N)orth or (S)outh.")
-        travel = input.lower("Direction")
+        travel = input("Direction ")
+        travel = travel.lower
+        if travel == "e" or travel == "w":
+            if travel == "e":
+                position += 10
+            elif travel == "w":
+                position -= 10
+        else:
+            print("not a valid direction!")
 
     elif position == 33:
         print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
+        travel = input("Direction ")
+        travel = travel.lower()
 
     elif position == 32:
         print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
+        travel = input("Direction ")
+        travel = travel.lower()
 
 
 print("Victory!")
