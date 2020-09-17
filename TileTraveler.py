12fe4a217e750c9a236dc56c3b54eaf3988eeba8
Robin <robin20@ru.is>
2020-09-17 10:46:38 +0000

    xx

--- TileTraveler.py
+++ TileTraveler.py
@@ -4,41 +4,44 @@ while position != 31:
     if position == 11:
         print("You can travel: (N)orth.")
         travel = input.lower("Direction")
-        if travel == "n":
+    elif travel == "n":
             position += 1
         else:
             print("Not a valid direction!")
-    if position == 12:
+    elif position == 12:
         print("You can travel: (N)orth or (E)ast or (S)outh.")
         travel = input.lower("Direction")
-        if travel == "n" or travel == "e" or travel == "s":
-            if travel == "n":
+        elif travel == "n" or travel == "e" or travel == "s":
+            elif travel == "n":
                 position +=1
 
         else:
             print("not a valid direction!")
 
-    if position == 13:
+    elif position == 13:
+        print("You can travel: (E)ast or (S)outh.")
+        travel = input.lower("Direction")
+        elif  travel == "e" or travel == "s":
+            elif travel == "n":
+                position +=1  
+
+    elif position == 21:
+        print("You can travel: (E)ast or (W)est.")
+        travel = input.lower("Direction")
+
+    elif position == 22:
+        print("You can travel: (S)outh or (W)est.")
+        travel = input.lower("Direction")
+
+    elif position == 23:
+        print("You can travel: (N)orth or (S)outh.")
+        travel = input.lower("Direction")
+
+    elif position == 33:
         print("You can travel: (N)orth.")
         travel = input.lower("Direction")
 
-    if position == 21:
-        print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
-
-    if position == 22:
-        print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
-
-    if position == 23:
-        print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
-
-    if position == 33:
-        print("You can travel: (N)orth.")
-        travel = input.lower("Direction")
-
-    if position == 32:
+    elif position == 32:
         print("You can travel: (N)orth.")
         travel = input.lower("Direction")
 
